15ad52cb2c5e45cb7054fffe4d7c51f4c93b0796
denebe <twhjdfo01@daum.net>
2022-11-16 17:42:12 +0900

    [feat]tailwind 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { searchApi } from "./api";
-import "./App.css";
+
 //RomvFYLUmsLT0K8f2ungzLsbWw3fnkq0ClZkN3mYE6ZIoDrjW0jDcIaEWZY1adNqDIYWNRnHhoF1Wg
 function App() {
   const [id, setId] = useState<string>("");
@@ -15,24 +15,46 @@ function App() {
   };
 
   return (
-    <>
-      <div className="App">
-        <input onChange={onChange} value={id} />
-        <button onClick={onClick}>검색</button>
+    <div className="relative px-6 lg:px-8">
+      <div className="mx-auto max-w-3xl pt-20 pb-12 sm:pt-48">
+        <div className="flex shadow-sm space-x-4 justify-center">
+          <input
+            className="block w-96 rounded-md border border-slate-300 border-gray-500 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            placeholder="소환사 이름 검색"
+            onChange={onChange}
+            value={id}
+          />
+          <button
+            className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+            onClick={onClick}
+          >
+            검색
+          </button>
+        </div>
       </div>
-      {db != null
-        ? db
-            .filter((i) => i.queueType == "RANKED_SOLO_5x5")
-            .map((i) => (
-              <div key={i.leagueId}>
-                닉네임: <h1>{i.summonerName}</h1>
-                티어: <h1>{i.tier}</h1>
-                랭크 : <h1>{i.rank}</h1>
-                포인트 : <h1>{i.leaguePoints}</h1>
-              </div>
-            ))
-        : "dd"}
-    </>
+      <div className="mx-auto  max-w-3xl justify-center ">
+        {db != null
+          ? db
+              .filter((i) => i.queueType == "RANKED_SOLO_5x5")
+              .map((i) => (
+                <div className="w-auto" key={i.leagueId}>
+                  <div className="flex justify-center">
+                    닉네임: <h1>{i.summonerName}</h1>
+                  </div>
+                  <div className="flex justify-center">
+                    티어: <h1>{i.tier}</h1>
+                  </div>
+                  <div className="flex justify-center">
+                    랭크 : <h1>{i.rank}</h1>
+                  </div>
+                  <div className="flex justify-center">
+                    포인트 : <h1>{i.leaguePoints}</h1>
+                  </div>
+                </div>
+              ))
+          : "dd"}
+      </div>
+    </div>
   );
 }
 
